aab00cd7c95dd8d31a1967b5246d04f12c770dd7
Samanyu Parvathaneni <psamanyu@gmail.com>
2025-11-29 07:52:41 -0600

    start over

--- src/run_scheduler.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-from agent.scheduler import start_scheduler
-
-logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(name)s: %(message)s")
-
-
-if __name__ == "__main__":
-    start_scheduler()
